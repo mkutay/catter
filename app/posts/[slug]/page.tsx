9a8f93e09239dc790151a9580695ae4cf39ddf94
mkutay <me@mkutay.dev>
2024-06-04 15:12:10 +0300

    about

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import MdxBuild from "@/app/lib/mdxBuild";
 import { getMDXComponent } from 'mdx-bundler/client';
 import path from 'path';
+import { getFormatter } from 'next-intl/server';
 // import * as React from 'react';
 
 // export default async function Page() {
@@ -9,16 +10,32 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const cwd = process.cwd();
   const { code, frontmatter } = await MdxBuild(slug, path.join(cwd, `app/posts/posts/`));
 
+  const format = await getFormatter();
+  const date = new Date(frontmatter.date);
+
+  const formattedDate = format.dateTime(date, {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
   // const Component = React.useMemo(() => getMDXComponent(code), [code]);
   const Component = getMDXComponent(code);
 
   return (
     <div className="max-w-prose mx-auto my-0 py-8">
       <header>
-        <h1 className="font-semibold text-4xl">{frontmatter.title}</h1>
-        <p>{frontmatter.description}</p>
+        <h1 className="font-bold text-xl pb-1.5">
+          {frontmatter.title}
+        </h1>
+        <p className="py-1.5 font-medium">
+          {formattedDate}
+        </p>
+        <p className="py-1.5 italic text-right">
+          {frontmatter.description}
+        </p>
       </header>
-      <main>
+      <main className="prose">
         <Component/>
       </main>
     </div>
