98788740e5b4a3552487565739f403af5772e72d
mkutay <me@mkutay.dev>
2024-06-06 20:13:30 +0300

    guestbook test

---
+++ app/lib/dataBaseQueries.tsx
@@ -0,0 +1,50 @@
+'use server';
+
+import { sql } from '@/app/lib/postgres';
+import {
+  unstable_cache as cache,
+  unstable_noStore as noStore,
+} from 'next/cache';
+
+
+export async function getBlogViews() {
+  if (!process.env.POSTGRES_URL) {
+    return [];
+  }
+
+  noStore();
+  let views = await sql`
+    SELECT count
+    FROM views
+  `;
+
+  return views.reduce((acc, curr) => acc + Number(curr.count), 0);
+}
+
+export async function getViewsCount(): Promise<
+  { slug: string; count: number }[]
+> {
+  if (!process.env.POSTGRES_URL) {
+    return [];
+  }
+
+  noStore();
+  return sql`
+    SELECT slug, count
+    FROM views
+  `;
+}
+
+export async function getGuestbookEntries() {
+  if (!process.env.POSTGRES_URL) {
+    return [];
+  }
+
+  noStore();
+  return sql`
+    SELECT id, body, created_by, updated_at
+    FROM guestbook
+    ORDER BY created_at DESC
+    LIMIT 100
+  `;
+}
