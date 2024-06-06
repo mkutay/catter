f5d5480ac6037c03a73c93151bf2ef6264dabbf3
mkutay <me@mkutay.dev>
2024-06-06 21:34:22 +0300

    guestbook test

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -16,7 +16,7 @@ export function SignOut() {
 export function SignIn() {
   return (
     <button
-      className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 mb-8"
+      className="border border-[#bcc0cc] dark:border-[#45475a] bg-[#ccd0da] dark:bg-[#313244] rounded p-4 not-prose text-sm inline-flex items-center leading-4 text-[#4c4f69] dark:text-[#cdd6f4] mb-8"
       onClick={() => signIn('github')}
     >
       <img alt="GitHub logo" src="/github-logo.svg" width="20" height="20" />
