03ab8ae211f3a2fee63085a49829e283fb630870
mkutay <me@mkutay.dev>
2024-06-16 13:18:54 +0300

    pagination for posts

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -20,7 +20,7 @@ export function SignIn() {
       className="w-full items-center justify-center border border-[#bcc0cc] dark:border-[#45475a] bg-[#e6e9ef] dark:bg-[#181825] rounded-md p-4 not-prose inline-flex text-[#4c4f69] dark:text-[#cdd6f4] mb-6"
       onClick={() => signIn('github')}
     >
-      <GithubSvg width={20} height={20}/>
+      <GithubSvg width={24} height={24}/>
       <div className="ml-4">Sign in with GitHub</div>
     </button>
   );
