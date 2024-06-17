5a1bbb70205abf8f0f1839d4f80517a3b494fcd5
mkutay <me@mkutay.dev>
2024-06-17 11:46:26 +0300

    rss feed

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -1,14 +1,13 @@
 import Rss from 'rss';
 import getPosts from '@/app/lib/getPosts';
 
-export async function GET() {
+export function GET() {
   const feed = new Rss({
     title: 'Kutay\'s Blog',
     description: 'A blog where uni student Kutay posts about things he likes, from maths to computer science',
     generator: 'RSS for Node and Next.js',
     feed_url: 'https://www.mkutay.dev/feed.xml',
     site_url: 'https://www.mkutay.dev/',
-    image_url: 'https://www.mkutay.dev/favicon.ico',
     managingEditor: 'hello@mkutay.dev (Mehmet Kutay Bozkurt)',
     webMaster: 'hello@mkutay.dev (Mehmet Kutay Bozkurt)',
     copyright: `Copyright ${new Date().getFullYear().toString()}, Mehmet Kutay Bozkurt`,
@@ -26,7 +25,7 @@ export async function GET() {
       url: `https://mkutay.dev/posts/${post.slug}`,
       date: post.meta.date,
       author: `Mehmet Kutay Bozkurt <hello@mkutay.dev>`,
-    })
+    });
   });
 
   return new Response(feed.xml(), {
