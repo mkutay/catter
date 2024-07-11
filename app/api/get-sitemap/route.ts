d62ec76dd985a63145823bc03f146ac68216cbeb
mkutay <me@mkutay.dev>
2024-07-11 16:13:03 +0300

    i quit sitemap doesnt generaste statically bRUH

--- app/api/get-sitemap/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import sitemap from "@/app/sitemap";
-
-export async function GET() {
-  const retSitemap = await sitemap();
-
-  return NextResponse.json(retSitemap);
-}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,10 +7,7 @@ import { getGuestbookEntries } from '@/app/lib/dataBaseQueries';
 import getProps from '@/app/lib/getProps';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const oldPosts = await (await fetch('https://mkutay.dev/api/get-posts')).json();
   const posts = getPosts({ });
-
-  const oldSitemap = await (await fetch('https://mkutay.dev/api/get-sitemap')).json();
 
   const guestbookEntries = await getGuestbookEntries();
   guestbookEntries.sort((a, b) => 
@@ -41,51 +38,20 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date(getProps('content/pages', 'about').meta.date),
   });
 
-  let oldSitemapPostsLastMod: Date = siteConfig.date;
-
-  oldSitemap.forEach((siteMapEl: any) => {
-    if (siteMapEl.url.includes('posts/page')) {
-      oldSitemapPostsLastMod = siteMapEl.lastModified;
-      return;
-    }
-  });
-
   for (let i = 1; i <= Math.ceil(posts.length / siteConfig.postNumPerPage); i++) {
     siteMap.push({
       url: `https://www.mkutay.dev/posts/page/${i}`,
-      lastModified: posts.length === oldPosts.length ? oldSitemapPostsLastMod : siteConfig.date,
+      lastModified: siteConfig.date,
     });
   }
 
   const tags = getListOfAllTags();
   tags.forEach((tag) => {
     const postsWithTagLength = getPostsLength(tag);
-
-    let oldPostsWithTag: {
-      meta: { [key: string]: any },
-      slug: string,
-      content: string,
-    }[] = [];
-    oldPosts.forEach((oldPost: any) => {
-      if (oldPost.meta.tags.includes(tag)) {
-        oldPostsWithTag.push(oldPost);
-      }
-    });
-    const oldPostsWithTagLength = oldPostsWithTag.length;
-
-    let oldSitemapPostsWithTagLastMod: Date = siteConfig.date;
-
-    oldSitemap.forEach((siteMapEl: any) => {
-      if (siteMapEl.url.includes(`tags/${tag}`)) {
-        oldSitemapPostsWithTagLastMod = siteMapEl.lastModified;
-        return;
-      }
-    });
-
     for (let i = 1; i <= Math.ceil(postsWithTagLength / siteConfig.postNumPerPage); i++) {
       siteMap.push({
         url: `https://www.mkutay.dev/tags/${tag}/page/${i}`,
-        lastModified: postsWithTagLength === oldPostsWithTagLength ? oldSitemapPostsWithTagLastMod : siteConfig.date,
+        lastModified: siteConfig.date,
       });
     }
   });
