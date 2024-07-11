57500ee7b1ca43d88d3db16bf1a5c4acd4f9162f
mkutay <me@mkutay.dev>
2024-07-11 16:27:17 +0300

    trying the oldest code

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,47 +1,51 @@
 import { MetadataRoute } from 'next';
-
 import getPosts, { getPostsLength } from '@/app/lib/getPosts';
-import { getListOfAllTags } from '@/app/lib/getListOfAllTags';
+import { getListOfAllTags } from './lib/getListOfAllTags';
 import { siteConfig } from '@/config/site';
-import { getGuestbookEntries } from '@/app/lib/dataBaseQueries';
-import getProps from '@/app/lib/getProps';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const guestbookEntries = await getGuestbookEntries();
-  guestbookEntries.sort((a, b) => 
-    new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-  );
-
+export default function sitemap(): MetadataRoute.Sitemap {
   const posts = getPosts({ });
+
+  const postsLength = posts.length;
 
   let siteMap = [];
 
   posts.forEach((post) => {
     siteMap.push({
-      url: `${siteConfig.url}/posts/${post.slug}`,
-      lastModified: new Date(String(post.meta.lastModified ?? post.meta.date)),
+      url: `https://www.mkutay.dev/posts/${post.slug}`,
+      lastModified: new Date(String(post.meta.date)),
+      changeFrequency: 'yearly',
+      priority: 0.75,
     });
   });
 
   siteMap.push({
-    url: siteConfig.url,
-    lastModified: siteConfig.date,
+    url: 'https://www.mkutay.dev',
+    lastModified: new Date(),
+    changeFrequency: "weekly" as const,
+    priority: 0.8,
   });
 
   siteMap.push({
-    url: `${siteConfig.url}/guestbook`,
-    lastModified: new Date(guestbookEntries[0].created_at),
+    url: 'https://www.mkutay.dev/guestbook',
+    lastModified: new Date(),
+    changeFrequency: "monthly" as const,
+    priority: 0.75,
   });
 
   siteMap.push({
-    url: `${siteConfig.url}/about`,
-    lastModified: new Date(getProps('content/pages', 'about').meta.date),
+    url: 'https://www.mkutay.dev/about',
+    lastModified: new Date(),
+    changeFrequency: "yearly" as const,
+    priority: 0.78,
   });
 
-  for (let i = 1; i <= Math.ceil(posts.length / siteConfig.postNumPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(postsLength / siteConfig.postNumPerPage); i++) {
     siteMap.push({
-      url: `${siteConfig.url}/posts/page/${i}`,
-      lastModified: siteConfig.date,
+      url: `https://www.mkutay.dev/posts/page/${i}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 0.6,
     });
   }
 
@@ -50,8 +54,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const postsWithTagLength = getPostsLength(tag);
     for (let i = 1; i <= Math.ceil(postsWithTagLength / siteConfig.postNumPerPage); i++) {
       siteMap.push({
-        url: `${siteConfig.url}/tags/${tag}/page/${i}`,
-        lastModified: siteConfig.date,
+        url: `https://www.mkutay.dev/tags/${tag}/page/${i}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly" as const,
+        priority: 0.62,
       });
     }
   });
