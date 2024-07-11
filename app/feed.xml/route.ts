9380ebbb1bf33ac31c368389d2fa45f1947d1324
mkutay <me@mkutay.dev>
2024-07-11 17:10:28 +0300

    fml

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -1,13 +1,14 @@
 import Rss from 'rss';
 import getPosts from '@/app/lib/getPosts';
+import { siteConfig } from '@/config/site';
 
 export async function GET() {
   const feed = new Rss({
     title: 'Kutay\'s Blog',
     description: 'A blog where uni student Kutay posts about things he likes, from maths to computer science',
     generator: 'RSS for Node and Next.js',
-    feed_url: 'https://www.mkutay.dev/feed.xml',
-    site_url: 'https://www.mkutay.dev/',
+    feed_url: `${siteConfig.url}/feed.xml`,
+    site_url: siteConfig.url,
     managingEditor: 'hello@mkutay.dev (Mehmet Kutay Bozkurt)',
     webMaster: 'hello@mkutay.dev (Mehmet Kutay Bozkurt)',
     copyright: `Copyright ${new Date().getFullYear().toString()}, Mehmet Kutay Bozkurt`,
@@ -22,7 +23,7 @@ export async function GET() {
     feed.item({
       title: post.meta.title,
       description: post.meta.description,
-      url: `https://mkutay.dev/posts/${post.slug}`,
+      url: `${siteConfig.url}/posts/${post.slug}`,
       date: post.meta.date,
       author: 'hello@mkutay.dev (Mehmet Kutay Bozkurt)',
       categories: post.meta.tags || [],
