79dbbd88ff3fe21ed90481c67af671a291da24c3
Mehmet Kutay Bozkurt <me@mkutay.dev>
2024-07-21 17:53:26 +0000

    main page change

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,25 @@ import ListPosts from '@/components/listPosts';
 
 export default function Home() {
   return (
-    <section className="max-w-prose mx-auto my-8 prose px-4 prose-h1:my-0">
-      <h1>
-        Latest Posts
-      </h1>
-      <hr/>
-      <ListPosts startInd={0} endInd={siteConfig.postNumPerPage}/>
-      <hr/>
-      <EmailSubButton/>
-      <hr/>
-      <h1 className="mb-6">
-        Top Posts
-      </h1>
-      <Suspense>
-        <MostViewedPosts postNum={Math.floor(siteConfig.postNumPerPage * 1.5)}/>
-      </Suspense>
+    <section className="justify-center my-8 px-4 prose-h1:my-0 flex lg:flex-row flex-col lg:gap-8 gap-0">
+      <div className="lg:w-2/3 prose w-full lg:mx-0 mx-auto">
+        <h1>
+          Latest Posts
+        </h1>
+        <hr/>
+        <ListPosts startInd={0} endInd={siteConfig.postNumPerPage}/>
+      </div>
+      <div className="lg:w-1/3 prose w-full lg:mx-0 mx-auto">
+        <hr className="lg:hidden flex"/>
+        <h1 className="mb-6">
+          Top Posts
+        </h1>
+        <Suspense>
+          <MostViewedPosts postNum={Math.floor(siteConfig.postNumPerPage * 1.5)}/>
+        </Suspense>
+        <hr/>
+        <EmailSubButton/>
+      </div>
     </section>
   )
 }
