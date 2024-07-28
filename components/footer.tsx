a758a6cd01cd9a555e4652764f2c42980a3b45a1
Mehmet Kutay Bozkurt <me@mkutay.dev>
2024-07-28 13:44:10 +0000

    finally im home

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,22 +7,22 @@ import { siteConfig } from '@/config/site';
 export default function Footer() {
   return (
     <footer className="items-center lg:items-start lg:px-8 px-4 my-12 flex lg:flex-row flex-col max-w-prose lg:max-w-6xl mx-auto gap-8 lg:gap-0">
-      <div className="lg:mr-auto flex flex-col gap-6">
+      <div className="lg:mr-auto flex flex-col h-full grow place-content-between">
         <div className="flex flex-col gap-4">
           <div className="text-lg">
-            Made with <b className="dark:text-[#f38ba8] text-[#d20f39]">&lt;3</b>
+            Made with <b className="text-red">&lt;3</b>
           </div>
           <p className="lg:flex hidden">
             Thanks for reading.
           </p>
         </div>
-        <p className="text-sm text-[#6c6f85] dark:text-[#a6adc8] lg:flex hidden">
+        <p className="text-sm text-subtext-0 lg:flex hidden mt-6">
           © 2023-present Mehmet Kutay Bozkurt. All Rights Reserved.
         </p>
       </div>
       <div className="lg:ml-auto flex flex-row gap-16">
         <div className="flex flex-col gap-6">
-          <p className="text-[#6c6f85] dark:text-[#a6adc8] text-sm">
+          <p className="text-subtext-0 text-sm">
             Connections
           </p>
           <div className="flex flex-col gap-3">
@@ -30,7 +30,7 @@ export default function Footer() {
               <Link
                 key={item.link}
                 href={item.link}
-                className="hover:text-[#1e66f5] hover:dark:text-[#89b4fa] hover:underline"
+                className="hover:text-blue hover:underline"
               >
                 {item.title}
               </Link>
@@ -38,7 +38,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex flex-col gap-6">
-          <p className="text-[#6c6f85] dark:text-[#a6adc8] text-sm">
+          <p className="text-subtext-0 text-sm">
             Blog
           </p>
           <div className="flex flex-col gap-3">
@@ -46,7 +46,7 @@ export default function Footer() {
               <Link
                 key={item.link}
                 href={item.link}
-                className="hover:text-[#1e66f5] hover:dark:text-[#89b4fa] hover:underline"
+                className="hover:text-blue hover:underline"
               >
                 {item.title}
               </Link>
@@ -54,7 +54,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <p className="text-sm text-[#6c6f85] dark:text-[#a6adc8] lg:hidden flex text-center justify-center">
+      <p className="text-sm text-subtext-0 lg:hidden flex text-center justify-center">
         © 2023-present Mehmet Kutay Bozkurt. All Rights Reserved.
       </p>
     </footer>
