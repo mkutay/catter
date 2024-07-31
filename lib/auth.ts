7e5109e8171e8fc57ba2112bb0d8cb45ef4494bc
mkutay <me@mkutay.dev>
2024-07-31 18:59:26 +0300

    spotify oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,24 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import GitHubProvider from 'next-auth/providers/github';
 import DiscordProvider from 'next-auth/providers/discord';
+import SpotifyProvider from 'next-auth/providers/spotify';
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
   providers: [
-    GitHub({
+    GitHubProvider({
       clientId: process.env.OAUTH_CLIENT_KEY as string,
       clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
     }),
     DiscordProvider({
       clientId: process.env.DISCORD_OAUTH_CLIENT_KEY as string,
       clientSecret: process.env.DISCORD_OAUTH_CLIENT_SECRET as string,
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_OAUTH_CLIENT_KEY as string,
+      clientSecret: process.env.SPOTIFY_OAUTH_CLIENT_SECRET as string,
     }),
   ],
 });
