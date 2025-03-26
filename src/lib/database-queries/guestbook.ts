8d86320c64bea1dce015afbc9302c65b4e5add3d
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-03-26 23:37:11 +0000

    refactor(database-queries): improve querying for guestbook in the db

--- src/lib/database-queries/guestbook.ts
+++ src/lib/database-queries/guestbook.ts
@@ -1,19 +1,23 @@
 'use server';
 
 import { sql } from '@/lib/postgres';
-import { CommentData, EntryData } from '@/config/types';
+import { EntryData } from '@/config/types';
 
-export async function getGuestbookEntries(): Promise<
-  EntryData[]
-> {
+export async function getGuestbookEntries(limit?: number): Promise<EntryData[]> {
   if (!process.env.POSTGRES_URL) {
-    return [];
+    throw new Error('Postgres database URL is not defined.');
+  }
+
+  limit = limit || 100;
+
+  if (limit <= 0 || limit > 200) {
+    throw new Error('Limit out of allowed range.');
   }
 
   return sql`
     SELECT id, body, created_by, created_at, updated_at, email, color
     FROM guestbook
     ORDER BY created_at DESC
-    LIMIT 300;
+    LIMIT ${limit};
   `;
 }
