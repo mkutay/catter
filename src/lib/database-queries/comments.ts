3fae969bda94bc08f506fb5821895f8f3ab81f78
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-03-26 23:35:28 +0000

    refactor(database-queries): improve db queries for comments

--- src/lib/database-queries/comments.ts
+++ src/lib/database-queries/comments.ts
@@ -1,13 +1,18 @@
 'use server';
 
 import { sql } from '@/lib/postgres';
+import { doesPostWithSlugExist } from '@/lib/contentQueries';
 import { CommentData } from '@/config/types';
 
-export async function getComments({ slug }: { slug: string }): Promise<
-  CommentData[]
-> {
+/* Limiting to 15 to avoid loading too many comments at once. */
+
+export async function getComments({ slug }: { slug: string }): Promise<CommentData[]> {
   if (!process.env.POSTGRES_URL) {
-    return [];
+    throw new Error('Postgres database URL not found.');
+  }
+
+  if (!doesPostWithSlugExist(slug)) {
+    throw new Error(`Post with slug ${slug} not found.`);
   }
   
   return sql`
@@ -19,17 +24,21 @@ export async function getComments({ slug }: { slug: string }): Promise<
   `;
 }
 
-export async function getEveryComment(): Promise<
-  CommentData[]
-> {
+export async function getEveryComment(limit?: number): Promise<CommentData[]> {
   if (!process.env.POSTGRES_URL) {
-    return [];
+    throw new Error('Postgres database URL not found.');
+  }
+
+  limit = limit || 15;
+
+  if (limit <= 0 || limit > 100) {
+    throw new Error('Limit must be between 1 and 100.');
   }
   
   return sql`
     SELECT id, slug, body, created_by, created_at, updated_at, email
     FROM comments
     ORDER BY created_at DESC
-    LIMIT 15;
+    LIMIT ${limit};
   `;
 }
