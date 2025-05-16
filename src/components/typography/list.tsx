86826bedb509a9d7dc199c5beed0078d370aead7
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-05-16 16:13:35 +0100

    fix: adjust list item margin for unordered and ordered lists

--- src/components/typography/list.tsx
+++ src/components/typography/list.tsx
@@ -3,7 +3,7 @@ import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 export function TypographyUList(props: DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement>) {
   return (
-    <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", props.className)} {...props}>
+    <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-0.5", props.className)} {...props}>
       {props.children}
     </ul>
   );
@@ -11,7 +11,7 @@ export function TypographyUList(props: DetailedHTMLProps<HTMLAttributes<HTMLULis
 
 export function TypographyOList(props: DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLOListElement>) {
   return (
-    <ol className={cn("my-6 ml-6 list-decimal [&>li]:mt-2", props.className)} {...props}>
+    <ol className={cn("my-6 ml-6 list-decimal [&>li]:mt-0.5", props.className)} {...props}>
       {props.children}
     </ol>
   );
