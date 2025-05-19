b3f82f2e56e12378b1b1e37b8e5a9b9ffebeea68
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-05-19 16:39:14 +0100

    chore: remove unnecessary stuff

--- src/config/images.tsx
+++ src/config/images.tsx
@@ -1,78 +1,9 @@
 import { StaticImageData } from 'next/image';
 
-import coverSkip from '@/public/images/skip-list/cover.jpeg';
-import coverClone from '@/public/images/creating-a-clone-of-yourself/cover.jpeg';
-import coverCatter from '@/public/images/catter-blog/cover.png';
-import coverArt from '@/public/images/how-to-practice-mathematics-as-an-art/cover.jpeg';
-import coverResults from '@/public/images/results-of-my-lifes-last-two-years-are-out/cover.jpeg';
-import coverSlang from '@/public/images/turkish-slang-dictionary/cover.png';
-import coverStartpage from '@/public/images/purple-startpage/cover.png';
-import coverHate from '@/public/images/why-do-people-just-hate-mathematics/cover-cropped.jpeg';
-import coverLonely from '@/public/images/why-mathematics-is-lonely/cover.jpeg';
-import coverLeaks from '@/public/images/ib-leaks/cover.jpeg';
-import coverProcrastination from '@/public/images/procrastination/cover.jpeg';
-import coverSquareSkip from '@/public/images/skip-list/cover-square.jpg';
-import coverSquareClone from '@/public/images/creating-a-clone-of-yourself/cover-cropped.jpg';
-import coverSquareCatter from '@/public/images/catter-blog/square.png';
-import coverSquareArt from '@/public/images/how-to-practice-mathematics-as-an-art/cover-square.jpg';
-import coverSquareResults from '@/public/images/results-of-my-lifes-last-two-years-are-out/cover-square.jpg';
-import coverSquareSlang from '@/public/images/turkish-slang-dictionary/square.png';
-import coverSquareStartpage from '@/public/images/purple-startpage/square.jpeg';
-import coverSquareHate from '@/public/images/why-do-people-just-hate-mathematics/cover-square.png';
-import coverSquareLonely from '@/public/images/why-mathematics-is-lonely/cover-square.jpg';
-import coverSquareLeaks from '@/public/images/ib-leaks/cover-square.jpg';
-import coverSquareProcrastination from '@/public/images/procrastination/cover-square.jpg';
 import followNextComeau from '@/public/follow-next-images/comeau.png';
 import followNextBendersky from '@/public/follow-next-images/bendersky.png';
 import followNextSophie from '@/public/follow-next-images/localghost.png';
 import followNextMelikechan from '@/public/follow-next-images/melikechan.png';
-import _images_skip_list_n_squared_insertion_png from '@/public/images/skip-list/n-squared-insertion.png';
-import _images_skip_list_example_situation_png from '@/public/images/skip-list/example-situation.png';
-import _images_skip_list_express_lane_png from '@/public/images/skip-list/express-lane.png';
-import _images_skip_list_first_look_at_the_structure_png from '@/public/images/skip-list/first-look-at-the-structure.png';
-import _images_skip_list_first_look_at_the_structure_search_path_png from '@/public/images/skip-list/first-look-at-the-structure-search-path.png';
-import _images_skip_list_structure_with_head_png from '@/public/images/skip-list/structure-with-head.png';
-import _images_skip_list_structure_with_layers_png from '@/public/images/skip-list/structure-with-layers.png';
-import _images_skip_list_structure_with_nodes_png from '@/public/images/skip-list/structure-with-nodes.png';
-import _images_skip_list_formal_search_path_png from '@/public/images/skip-list/formal-search-path.png';
-import _images_skip_list_list_interface_png from '@/public/images/skip-list/list-interface.png';
-import _images_creating_a_clone_of_yourself_message_chain_png from '@/public/images/creating-a-clone-of-yourself/message-chain.png';
-import _images_results_of_my_lifes_last_two_years_are_out_maths_results_png from '@/public/images/results-of-my-lifes-last-two-years-are-out/maths-results.png';
-import _images_results_of_my_lifes_last_two_years_are_out_physics_results_png from '@/public/images/results-of-my-lifes-last-two-years-are-out/physics-results.png';
-import _images_results_of_my_lifes_last_two_years_are_out_english_results_png from '@/public/images/results-of-my-lifes-last-two-years-are-out/english-results.png';
-import _images_purple_startpage_full_screenshot_jpeg from '@/public/images/purple-startpage/full-screenshot.jpeg';
-import _images_why_do_people_just_hate_mathematics_three_types_of_people_png from '@/public/images/why-do-people-just-hate-mathematics/three-types-of-people.png';
-import _images_why_do_people_just_hate_mathematics_triangle_rectangle_png from '@/public/images/why-do-people-just-hate-mathematics/triangle-rectangle.png';
-import _images_why_mathematics_is_lonely_one_sentence_proof_png from '@/public/images/why-mathematics-is-lonely/one-sentence-proof.png';
-import _images_javascript_and_asynchronous_functions_no_output_png from '@/public/images/javascript-and-asynchronous-functions/no-output.png';
-
-export const images: { [key: string]: StaticImageData } = {
-  'skip-list': coverSkip,
-  'creating-a-clone-of-yourself': coverClone,
-  'catter-blog': coverCatter,
-  'how-to-practice-mathematics-as-an-art': coverArt,
-  'results-of-my-lifes-last-two-years-are-out': coverResults,
-  'turkish-slang-dictionary': coverSlang,
-  'purple-startpage': coverStartpage,
-  'why-do-people-just-hate-mathematics': coverHate,
-  'why-mathematics-is-lonely': coverLonely,
-  'ib-leaks': coverLeaks,
-  'procrastination': coverProcrastination,
-};
-
-export const squareImages: { [key: string]: StaticImageData } = {
-  'skip-list': coverSquareSkip,
-  'creating-a-clone-of-yourself': coverSquareClone,
-  'catter-blog': coverSquareCatter,
-  'how-to-practice-mathematics-as-an-art': coverSquareArt,
-  'results-of-my-lifes-last-two-years-are-out': coverSquareResults,
-  'turkish-slang-dictionary': coverSquareSlang,
-  'purple-startpage': coverSquareStartpage,
-  'why-do-people-just-hate-mathematics': coverSquareHate,
-  'why-mathematics-is-lonely': coverSquareLonely,
-  'ib-leaks': coverSquareLeaks,
-  'procrastination': coverSquareProcrastination,
-};
 
 export const followNextImages: { [key: string]: StaticImageData } = {
   'comeau': followNextComeau,
@@ -80,25 +11,3 @@ export const followNextImages: { [key: string]: StaticImageData } = {
   'sophie': followNextSophie,
   'melikechan': followNextMelikechan,
 };
-
-export const postImages: { [key: string]: StaticImageData } = {
-  '/images/skip-list/n-squared-insertion.png': _images_skip_list_n_squared_insertion_png,
-  '/images/skip-list/example-situation.png': _images_skip_list_example_situation_png,
-  '/images/skip-list/express-lane.png': _images_skip_list_express_lane_png,
-  '/images/skip-list/first-look-at-the-structure.png': _images_skip_list_first_look_at_the_structure_png,
-  '/images/skip-list/first-look-at-the-structure-search-path.png': _images_skip_list_first_look_at_the_structure_search_path_png,
-  '/images/skip-list/structure-with-head.png': _images_skip_list_structure_with_head_png,
-  '/images/skip-list/structure-with-layers.png': _images_skip_list_structure_with_layers_png,
-  '/images/skip-list/structure-with-nodes.png': _images_skip_list_structure_with_nodes_png,
-  '/images/skip-list/formal-search-path.png': _images_skip_list_formal_search_path_png,
-  '/images/skip-list/list-interface.png': _images_skip_list_list_interface_png,
-  '/images/creating-a-clone-of-yourself/message-chain.png': _images_creating_a_clone_of_yourself_message_chain_png,
-  '/images/results-of-my-lifes-last-two-years-are-out/maths-results.png': _images_results_of_my_lifes_last_two_years_are_out_maths_results_png,
-  '/images/results-of-my-lifes-last-two-years-are-out/physics-results.png': _images_results_of_my_lifes_last_two_years_are_out_physics_results_png,
-  '/images/results-of-my-lifes-last-two-years-are-out/english-results.png': _images_results_of_my_lifes_last_two_years_are_out_english_results_png,
-  '/images/purple-startpage/full-screenshot.jpeg': _images_purple_startpage_full_screenshot_jpeg,
-  '/images/why-do-people-just-hate-mathematics/three-types-of-people.png': _images_why_do_people_just_hate_mathematics_three_types_of_people_png,
-  '/images/why-do-people-just-hate-mathematics/triangle-rectangle.png': _images_why_do_people_just_hate_mathematics_triangle_rectangle_png,
-  '/images/why-mathematics-is-lonely/one-sentence-proof.png': _images_why_mathematics_is_lonely_one_sentence_proof_png,
-  '/images/javascript-and-asynchronous-functions/no-output.png': _images_javascript_and_asynchronous_functions_no_output_png,
-}
