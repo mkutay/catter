8c342133963ec0ccff69eef77666e12a468c227b
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-03-11 19:00:07 +0000

    fix: add height parameter to image loader and conditionally set loader configuration

--- next-images-loader.js
+++ next-images-loader.js
@@ -1,17 +1,17 @@
 "use client";
 
-export default function myImageLoader({ src, width, quality }) {
+export default function myImageLoader({ src, width, quality, height }) {
   const isLocal = !src.startsWith("http");
   const query = new URLSearchParams();
 
   const imageOptimizationApi = "https://images.mkutay.dev";
-  // Your NextJS application URL
   const baseUrl = "https://www.mkutay.dev";
 
   const fullSrc = `${baseUrl}${src}`;
 
   if (width) query.set("width", width);
   if (quality) query.set("quality", quality);
+  if (height) query.set("height", height);
 
   if (isLocal && process.env.NODE_ENV === "development") {
     return src;
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
 import withPlaiceholder from "@plaiceholder/next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   turbopack: {},
   images: {
-    loader: "custom",
-    loaderFile: "./next-images-loader.js",
     qualities: [75],
+    ...(isProduction
+      ? {
+          loader: "custom",
+          loaderFile: "./next-images-loader.js",
+        }
+      : {}),
   },
 };
+
 export default withPlaiceholder(nextConfig);
