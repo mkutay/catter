99db9800011c097c6f2eefb6c836054dc7254d3f
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-03-13 01:28:50 +0000

    refactor: update list component styles

--- src/components/typography/list.tsx
+++ src/components/typography/list.tsx
@@ -7,7 +7,10 @@ export function TypographyUList(
 ) {
   return (
     <ul
-      className={cn("my-6 ml-6 list-disc [&>li]:mt-1", props.className)}
+      className={cn(
+        "my-6 ml-6 list-disc space-y-2 **:[&_ul]:my-2 **:[&_ol]:my-2 **:[&_ul]:ml-8 **:[&_ol]:ml-8",
+        props.className,
+      )}
       {...props}
     >
       {props.children}
@@ -21,7 +24,7 @@ export function TypographyOList(
   return (
     <ol
       className={cn(
-        "my-6 ml-6 list-decimal [&>li]:mt-1 [&>li]:pl-0.5",
+        "my-6 ml-6 list-decimal space-y-2 **:[&_ol]:my-2 **:[&_ul]:my-2 **:[&_ul]:ml-8 **:[&_ol]:ml-8",
         props.className,
       )}
       {...props}
