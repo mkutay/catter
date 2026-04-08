145e1e24a9f60f54ed5f9f427540d179af3632ac
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-04-08 18:18:43 +0400

    refactor: update the colour of the blockquote

--- src/components/typography/blockquote.tsx
+++ src/components/typography/blockquote.tsx
@@ -10,7 +10,10 @@ export function TypographyBlockquote(
 ) {
   return (
     <blockquote
-      className={cn("mt-6 border-l-2 pl-6 italic", props.className)}
+      className={cn(
+        "mt-6 border-l-2 pl-6 italic border-border",
+        props.className,
+      )}
       {...props}
     >
       {props.children}
