a3e1e3f4e055b0237820aa7d078ee8b42782c8d6
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-05-02 18:48:20 +0100

    fix: update button component with sizes lg/xl

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,7 +24,8 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-12 rounded-lg px-6 text-lg",
+        xl: "h-12 rounded-lg px-6 text-lg",
+        lg: "h-11 rounded-lg px-4 text-lg",
         icon: "h-9 w-9",
         md: "h-10 rounded-md px-3 text-md",
       },
