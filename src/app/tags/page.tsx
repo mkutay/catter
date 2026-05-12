9994c640e8aed2e326e6bf97aefa85de641e161b
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-05-12 18:48:28 +0100

    chore: move the TagsButtonGrid component directly into where its used

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import TagsButtonGrid from "@/components/tagsButtonGrid";
+import Link from "next/link";
 import { TypographyH1 } from "@/components/typography/headings";
+import { Button } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
+import { getListOfAllTags } from "@/lib/dbContentQueries";
+import { turnTagString } from "@/lib/utils";
 
 export const dynamic = "force-static";
 
@@ -16,13 +19,23 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
+export default async function Page() {
+  const tags = await getListOfAllTags();
+  if (tags.isErr()) throw new Error(tags.error.message);
   return (
     <>
       <TypographyH1 className="mt-6 mb-8 text-primary">
         List of Tags on the Blog
       </TypographyH1>
-      <TagsButtonGrid />
+      <div className="flex flex-col gap-6">
+        <div className="gap-2 grid grid-flow-row sm:grid-cols-3 grid-cols-2 items-center">
+          {tags.value.map((tag) => (
+            <Button key={tag} variant="outline" asChild className="uppercase">
+              <Link href={`/tags/${tag}`}>{turnTagString(tag)}</Link>
+            </Button>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
