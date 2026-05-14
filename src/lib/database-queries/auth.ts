42a9d81192e8e045f7246faae137b49c5add5cb5
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-05-14 20:56:38 +0100

    fix: update auth getters

--- src/lib/database-queries/auth.ts
+++ src/lib/database-queries/auth.ts
@@ -2,15 +2,14 @@ import { errAsync, okAsync, ResultAsync } from "neverthrow";
 import type { AuthError } from "@/config/types";
 import { auth } from "@/lib/auth";
 
+/**
+ * Retrieves the authenticated session.
+ *
+ * @returns A `ResultAsync` containing the session or an error.
+ * @note The if the session doesn't exists, it returns an error.
+ */
 export const getAuth = () =>
-  ResultAsync.fromPromise(
-    auth(),
-    () =>
-      ({
-        message: "Failed to get auth session.",
-        code: "AUTH_ERROR",
-      }) as AuthError,
-  ).andThen((session) =>
+  getSession().andThen((session) =>
     session
       ? okAsync(session)
       : errAsync({
@@ -19,6 +18,12 @@ export const getAuth = () =>
         } as AuthError),
   );
 
+/**
+ * Retrieves the authenticated session.
+ *
+ * @returns A `ResultAsync` containing the session or an error.
+ * @note The session might be `null`.
+ */
 export const getSession = () =>
   ResultAsync.fromPromise(
     auth(),
