3da7f4c1d47e04dc160c1eb2197f5fa6bf8994b1
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-05-14 23:04:03 +0100

    chore: run the formatter

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -142,11 +142,11 @@ CommandShortcut.displayName = "CommandShortcut";
 export {
   Command,
   CommandDialog,
-  CommandInput,
-  CommandList,
   CommandEmpty,
   CommandGroup,
+  CommandInput,
   CommandItem,
-  CommandShortcut,
+  CommandList,
   CommandSeparator,
+  CommandShortcut,
 };
