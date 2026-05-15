457a3aab0cfc852ee04fb79d0790c89144168fc4
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-05-15 17:51:38 +0100

    fix: update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: [".env.local", ".env"] });
+config({ path: [".env.local", ".env", ".env.development"] });
 
 const postgresURL = process.env.POSTGRES_URL;
 
